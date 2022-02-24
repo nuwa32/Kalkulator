6a7e493655717d9e219c18159b6d7f7932231a80
nuwa32 <konri41@gmail.com>
2022-02-24 12:11:50 +0100

    .

--- src/main/java/org/ks/kalkulator/Display.java
+++ src/main/java/org/ks/kalkulator/Display.java
@@ -5,14 +5,17 @@ import java.awt.*;
 
 public class Display extends JPanel {
     public double num1=0,num2=0,result=0;
-    public JLabel label;
+    public JLabel label,label1,tmp;
     /*public void addContent(String string){
         label.setText(label.getText()+string);
     }*/
     public Display() {
         super();
         label = new JLabel("");
+        label1 = new JLabel("");
+        add(label1);
         add(label);
         setPreferredSize(new Dimension(400, 30));
     }
+
 }
--- src/main/java/org/ks/kalkulator/Frame.java
+++ src/main/java/org/ks/kalkulator/Frame.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Frame extends JFrame implements ActionListener {
+    int flag = 0,minFlag = 0;
     //    private String getExpresionasString(){
 //        return (display.num1+"") + keyboard.operator + (display.num2+"");
 //    }
@@ -25,22 +26,34 @@ public class Frame extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
         for (int i = 0; i < 10; i++) {
+            if(flag==1){
+            flag=0;
+            display.label.setText(String.valueOf(display.num1));
+            }
             if (e.getSource() == keyboard.nButtons[i]) {
                 display.label.setText(display.label.getText().concat(String.valueOf(i)));
+                if(minFlag==0){
+                    minFlag=1;
+                }
             }
+
         }
         if (e.getSource() == keyboard.przecinek) {
             display.label.setText(display.label.getText().concat("."));
         }
         if (e.getSource() == keyboard.dodaj) {
-            display.num1 = Double.parseDouble(display.label.getText());
-            keyboard.operator = '+';
-            display.label.setText("");
+                display.num1 = Double.parseDouble(display.label.getText());
+                keyboard.operator = '+';
+                display.label.setText("");
         }
         if (e.getSource() == keyboard.odejmij) {
-            display.num1 = Double.parseDouble(display.label.getText());
-            keyboard.operator = '-';
-            display.label.setText("");
+            if(minFlag==0){
+                display.label.setText(display.label.getText().concat(String.valueOf("-")));
+            }else{
+                display.num1 = Double.parseDouble(display.label.getText());
+                keyboard.operator = '-';
+                display.label.setText("");
+            }
         }
         if (e.getSource() == keyboard.pomnoz) {
             display.num1 = Double.parseDouble(display.label.getText());
@@ -59,31 +72,38 @@ public class Frame extends JFrame implements ActionListener {
             if (keyboard.operator == '^') {
                 display.num1 = Double.parseDouble(display.label.getText());
                 display.result = display.num1 * display.num1;
+                display.label.setText(String.valueOf(display.num1)+keyboard.operator+display.num1+"="+display.result);
             } else if (keyboard.operator == '√') {
                 display.num1 = Double.parseDouble(display.label.getText());
                 display.result = Math.sqrt(display.num1);
+                display.label.setText(keyboard.operator+String.valueOf(display.num1)+"="+display.result);
             } else {
                 display.num2 = Double.parseDouble(display.label.getText());
                 if(keyboard.operator2 == '%'){
                     display.num2 = ((display.num2/100)*(display.num1));
+                    display.label.setText(String.valueOf(display.num1)+keyboard.operator+display.num2+"="+display.result);
                 }
                 switch (keyboard.operator) {
-                    case '+':
+                    case '+' -> {
                         display.result = display.num1 + display.num2;
-                        break;
-                    case '-':
+                        display.label.setText(String.valueOf(display.num1) + keyboard.operator + display.num2 + "=" + display.result);
+                    }
+                    case '-' -> {
                         display.result = display.num1 - display.num2;
-                        break;
-                    case '*':
+                        display.label.setText(String.valueOf(display.num1) + keyboard.operator + display.num2 + "=" + display.result);
+                    }
+                    case '*' -> {
                         display.result = display.num1 * display.num2;
-                        break;
-                    case '/':
+                        display.label.setText(String.valueOf(display.num1) + keyboard.operator + display.num2 + "=" + display.result);
+                    }
+                    case '/' -> {
                         display.result = display.num1 / display.num2;
-                        break;
+                        display.label.setText(String.valueOf(display.num1) + keyboard.operator + display.num2 + "=" + display.result);
+                    }
                 }
             }
-            display.label.setText(String.valueOf(display.result));
             display.num1 = display.result;
+            flag=1;
         }
         if (e.getSource() == keyboard.pierwiastek) {
             keyboard.operator = '√';
@@ -92,10 +112,14 @@ public class Frame extends JFrame implements ActionListener {
             keyboard.operator = '^';
         }
         if (e.getSource() == keyboard.wyczysc) {
+            flag = 0;
+            display.num2 = 0;
+            display.num1 = 0;
+            display.result = 0;
             display.label.setText("");
+
         }
     }
-
     private Display display;
     private Keyboard keyboard;
 }
